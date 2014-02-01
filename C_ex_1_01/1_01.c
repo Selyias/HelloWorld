56ea249448de25229bfd2a1d83622ae50e1a89af
Selyias <Selyias@gmail.com>
2014-02-01 02:29:39 -0800

    Chapter 1, Exercise 1-01

---
+++ C_ex_1_01/1_01.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   1_01.c
+ * Author: selyias
+ *
+ * Created on February 1, 2014, 1:48 AM
+ */
+
+#include <stdio.h>
+
+/*
+ * 
+ */
+main() 
+{
+
+    printf("hello, world\n");
+    return 0;
+}
+
