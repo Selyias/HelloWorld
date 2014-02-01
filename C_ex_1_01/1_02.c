10a61a7012161d6eec981a6b229a2668a42b9459
Selyias <Selyias@gmail.com>
2014-02-01 02:46:53 -0800

    Chapter 1, Exercise 1-01 after realizing I need to make programs separately.

---
+++ C_ex_1_01/1_02.c
@@ -0,0 +1,19 @@
+
+/* 
+ * File:   1_02.c
+ * Author: selyias
+ *
+ * Created on February 1, 2014, 1:48 AM
+ */
+
+#include <stdio.h>
+
+/*
+ * 
+ */
+main() 
+{
+
+    printf("hello, world\n");
+    return 0;
+}
---
+++ C_ex_1_01/build/Debug/Cygwin_4.x-Windows/1_02.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin_4.x-Windows/1_02.o: 1_02.c
