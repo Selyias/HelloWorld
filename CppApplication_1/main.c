7c47747b4ad71bcb2d69210c3a09701370aa0d9e
Selyias <Selyias@gmail.com>
2014-02-01 01:41:07 -0800

    NetBeans gitHub Commit compatibility test #1

---
+++ CppApplication_1/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: selyias
+ *
+ * Created on February 1, 2014, 1:20 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("howdy", stderr);
+    getchar();
+    
+    return (EXIT_SUCCESS);
+}
+
